678f66b31f0a328cdd0dab06450eb2fa695f1f2f
dasyps <syps@hotmail.com>
2018-09-30 00:31:44 -0500

    h

--- machine_analyse.py
+++ machine_analyse.py
@@ -1,4 +1,5 @@
 import psutil
+import sqlite3
 
 def k_to_gb(num):
     num /= 1024.0
@@ -24,11 +25,19 @@ def get_cpudata():
     idle = cpu_info.idle % 100
     return user,system,idle
 
+def create_mon_db():
+    if db file exists:
+        dont create_mon_db
+    else:
+        create database
+
+def create_mon_db_conn():
+
 def main():
     total,available,percent,used = get_memdata()
     user,system,idle = get_cpudata()
     print(f"CPU:Idle={idle:.2f}|User={user:.2f}|System={system:.2f}")
     print(f"MEM:Total={total:.2f}|Avail={available:.2f}|Used={used:.2f}|Percent={percent:.2f}")
-    
+
 if __name__ == '__main__':
     main()
